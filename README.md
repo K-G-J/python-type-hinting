1855bffb60694c49baded54f3f0eef4a2f0f75f9
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-22 17:23:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PYTHON TYPE-HINTING EXAMPLES
+
+## Description
+
+Example python files that utilize structural typing
+
+## Technologies Used
+
+- numbers (Real)
+- typing (Union, Optional, List)
---
+++ calculator.pyi
@@ -0,0 +1,13 @@
+from numbers import Real
+
+
+def add(num1: Real, num2: Real) -> Real
+
+
+def subtract(num1: Real, num2: Real) -> Real
+
+
+def multiply(num1: Real, num2: Real) -> Real
+
+
+def divide(num1: Real, num2: Real) -> Real
